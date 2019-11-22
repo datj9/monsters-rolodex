ab3a6554b429e25c5679660e8fa439d2aeb37ce2
Dat Nguyen <tandat198@gmail.com>
2019-11-22 08:18:33 +0700

    Updates

---
+++ precache-manifest.c595a1b5955d0eaa7e95a9970979e231.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649bc7aae69192eabf05918c3ff0f9db",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "484b9e43e7443b250be4",
+    "url": "/monsters-rolodex/static/css/main.e218fb4b.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "484b9e43e7443b250be4",
+    "url": "/monsters-rolodex/static/js/main.e99682dc.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
